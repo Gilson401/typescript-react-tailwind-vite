2d03d155b8f74c11143af0d10c1798bba7a2dfb7
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-07-31 15:08:16 -0300

    refcator(app):new text

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,54 @@
-import Avatar from 'components/Avatar'
+const links = [
+  {
+    label: 'Site Pessoal',
+    link: 'https://gilsonpaulo.com.br'
+  },
+
+  {
+    label: 'Linkedin',
+    link: 'https://www.linkedin.com/in/gpsc/'
+  },
+
+  {
+    label: 'Gitlab',
+    link: 'https://gitlab.com/GilsonInfet'
+  },
+
+  {
+    label: 'Github',
+    link: 'https://github.com/Gilson401'
+  }
+]
 
 function App() {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-screen-xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-screen-xl ">
+        <h1 className="h-20 bg-green-900 px-4 text-center text-4xl font-bold text-white sm:px-6 sm:text-5xl sm:tracking-tight lg:px-8 lg:text-6xl">
+          Reactjs Typescript Tailwindcss Vite Project
+        </h1>
         <div className="text-center">
-          <h2 className="text-base font-semibold uppercase tracking-wide text-blue-600">
-            Welcome to
+          <h2 className="mt-3 text-base font-semibold uppercase tracking-wide text-blue-600">
+            Hello Typescript!!!!
           </h2>
-          <p className="my-3 text-4xl font-bold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            reactjs-vite-tailwindcss-boilerplate
+
+          <p>This is my first project with typescript. Currently WIP </p>
+          <p>
+            Here you will find demonstrations of my coding skills with the above
+            stacks. Stay tunned for the next updates.
           </p>
-          <p className="text-xl text-gray-400">Start building for free.</p>
-          <p className="mt-5">
-            <Avatar
-              size="large"
-              src="https://www.gravatar.com/avatar/4405735f6f3129e0286d9d43e7b460d0"
-            />
-          </p>
+
+          <p className="mt-2 text-xl text-gray-400">Links:</p>
+
+          {links.map((link, index) => (
+            <a
+              className="mt-2 block bg-green-700 text-white"
+              key={index}
+              href={link.link}
+            >
+              {link.label}
+            </a>
+          ))}
         </div>
       </div>
     </div>
